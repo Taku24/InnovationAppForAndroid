241a1f8397d367b7f960f6f0b1d2f7a45297cf51
Taku24 <taku178.24@hotmail.co.jp>
2016-12-01 10:04:25 +0900

    fix code and add dialog

--- app/src/main/java/innovation/taku/com/inovationapp/Activity/CompanyActivity.java
+++ app/src/main/java/innovation/taku/com/inovationapp/Activity/CompanyActivity.java
@@ -1,4 +1,4 @@
-package innovation.taku.com.inovationapp;
+package innovation.taku.com.inovationapp.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -6,20 +6,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import innovation.taku.com.inovationapp.R;
+
 /**
  * Created by TAKU on 2016/11/30.
  */
 public class CompanyActivity extends AppCompatActivity implements View.OnClickListener{
 
+    @BindView(R.id.next)
     Button mNext;
+
+    @BindView(R.id.back)
     Button mBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company);
-        mNext = (Button)findViewById(R.id.next);
-        mBack = (Button)findViewById(R.id.back);
+        ButterKnife.bind(this);
         mNext.setOnClickListener(this);
         mBack.setOnClickListener(this);
     }
--- app/src/main/java/innovation/taku/com/inovationapp/Activity/MainActivity.java
+++ app/src/main/java/innovation/taku/com/inovationapp/Activity/MainActivity.java
@@ -1,4 +1,4 @@
-package innovation.taku.com.inovationapp;
+package innovation.taku.com.inovationapp.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,6 +12,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import innovation.taku.com.inovationapp.R;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
